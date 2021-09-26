a3ed49bad46b0f968c161899f651f7b9ae9720db
Salman Khan <kay2dan@gmail.com>
2021-09-26 17:00:08 +0500

    logic implementation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
 import { useState } from "react";
 import "./App.css";
 
-const Checkout = ({ cart, rules }) => {
-  // TODO: Implement me.
-  const total = 0;
+const Checkout = ({ cart, rules, prices }) => {
+  const cartItems = Object.keys(cart);
+  const originalPrice = cartItems.reduce(
+    (ttl, item) => ttl + cart[item] * prices[item],
+    0
+  );
 
+  let total = 0;
+  cartItems.forEach((item) => {
+    total = total + rules[item](cart[item], prices[item]);
+  });
+
+  // I would break the item presentation wise into smaller components
   return (
-    <table className="totals">
-      <tbody>
-        <tr>
-          <th scope="row">Total</th>
-          <td data-testid="total">{total}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="total">
+      {cartItems.map((item, i) => {
+        return (
+          <div className="" key={i}>
+            <span className="">{`${item} x ${cart[item]} :`}</span>
+            <span className=""></span>
+          </div>
+        );
+      })}
+      <h2>{`Total: ${total}`}</h2>
+      <p
+        style={{
+          color: "red",
+        }}
+      >
+        {`Original price: ${originalPrice}.
+        A saving of ${originalPrice - total}`}
+      </p>
+    </div>
   );
 };
 
 const App = () => {
+  // 1 - I wonder if, the ruleset and the item should be banded together,
+  // considering how interconnected they are.
+  // 2 - As the app grofs, I would definitely recoomend a state management tool,
+  // Im a fan of recoil for its 1-1 state objects, where the state is not global
   const [cart, setCart] = useState({
     bulb: 0,
     chess: 0,
     plant: 0,
   });
-  const [rules, setRules] = useState();
+  const prices = {
+    bulb: 3.11,
+    chess: 5,
+    plant: 11.23,
+  };
 
-  // const addLightBulb = () => {
-  // };
-
-  // const addChessSet = () => {
-  // };
-
-  // const addHousePlant = () => {
-  // };
+  // In hindsight, as apposed to current set rules for bulb & chess:
+  // - I would standardise the rules. i.e. define a rule called 'BOGOF',
+  // another for the 3 items prices reduction etc
+  const rules = {
+    bulb: (quantity, price) => {
+      return (quantity % 2) * price + Math.floor(quantity / 2) * price;
+    },
+    chess: (quantity, price) => {
+      const discPrice = 4.5;
+      return quantity >= 3 ? discPrice * quantity : price * quantity;
+    },
+    plant: (quantity, price) => {
+      return quantity * price;
+    },
+  };
 
   const addItem = (item) => {
     setCart({
@@ -48,7 +83,7 @@ const App = () => {
         <button onClick={() => addItem("chess")}>Chess set</button>
         <button onClick={() => addItem("plant")}>House plant</button>
       </div>
-      <Checkout cart={cart} rules={rules} />
+      <Checkout cart={cart} rules={rules} prices={prices} />
     </div>
   );
 };
